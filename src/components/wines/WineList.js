9b46ebd29f03cb7a8cebfc46008df2ad0ba6b499
Student <example@example.com>
2019-05-19 16:09:58 -0700

    create Wine Summary Component

--- src/components/wines/WineList.js
+++ src/components/wines/WineList.js
@@ -1,32 +1,14 @@
 import React from 'react'
+import WineSummary from './WineSummary'
 
 const WineList = () => {
   return (
     <div className="project-list section">
+      <WineSummary/>
+      <WineSummary/>
+      <WineSummary/>
+      <WineSummary/>
 
-      <div className="card x-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Wine Name</span>
-          <p>Posted by Victoria Martinez</p>
-          <p className="grey-text">June 1, 2am</p>
-        </div>
-      </div>
-
-      <div className="card x-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Wine Name</span>
-          <p>Posted by Victoria Martinez</p>
-          <p className="grey-text">June 1, 2am</p>
-        </div>
-      </div>
-
-      <div className="card x-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Wine Name</span>
-          <p>Posted by Victoria Martinez</p>
-          <p className="grey-text">June 1, 2am</p>
-        </div>
-      </div>
 
     </div>
   )
---
+++ src/components/wines/WineSummary.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const WineSummary = () => {
+  return(
+    <div className="card x-depth-0 project-summary">
+      <div className="card-content grey-text text-darken-3">
+        <span className="card-title">Wine Name</span>
+        <p>Posted by Victoria Martinez</p>
+        <p className="grey-text">June 1, 2am</p>
+      </div>
+    </div>
+  )
+}
+
+export default WineSummary
