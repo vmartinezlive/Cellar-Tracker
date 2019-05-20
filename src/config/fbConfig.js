365d090e4168b7df977e7478a0b7c21aa1ebd83f
Student <example@example.com>
2019-05-20 15:28:47 -0700

    add firebase to project

---
+++ src/config/fbConfig.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+
+// Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyA-bzvr1WFKs00k2LSz-UfyNcu89aaJtbM",
+    authDomain: "cellar-tracker-1ab8d.firebaseapp.com",
+    databaseURL: "https://cellar-tracker-1ab8d.firebaseio.com",
+    projectId: "cellar-tracker-1ab8d",
+    storageBucket: "cellar-tracker-1ab8d.appspot.com",
+    messagingSenderId: "887232062449",
+    appId: "1:887232062449:web:49f24052adf72f7e"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.firestore().settings({timestampsInSnapshots: true})
+
+export default firebase;
--- src/store/actions/wineActions.js
+++ src/store/actions/wineActions.js
@@ -1,5 +1,5 @@
 export const createWine = (wine) => {
-  return (dispatch, getState) => {
+  return (dispatch, getState, { getFirebase, getFirestore }) => {
     //make async call to database
     dispatch({type: 'CREATE_WINE', wine: wine });
   }
