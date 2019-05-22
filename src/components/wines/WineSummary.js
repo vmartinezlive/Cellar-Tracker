5cdcaa2a451eded4b69de1a253475ccb0ece5140
Student <example@example.com>
2019-05-21 21:25:30 -0700

    add user profile for new wines

--- src/components/wines/WineSummary.js
+++ src/components/wines/WineSummary.js
@@ -6,7 +6,7 @@ const WineSummary = ({wine}) => {
     <div className="card x-depth-0 project-summary">
       <div className="card-content grey-text text-darken-3">
         <span className="card-title">{wine.winename}</span>
-        <p>Posted by Victoria Martinez</p>
+        <p>Posted by {wine.authorFirstName} {wine.authorLastName}</p>
         <p className="grey-text">June 1, 2am</p>
       </div>
     </div>
--- src/store/actions/wineActions.js
+++ src/store/actions/wineActions.js
@@ -2,11 +2,13 @@ export const createWine = (wine) => {
   return (dispatch, getState, { getFirebase, getFirestore }) => {
     //make async call to database
     const firestore = getFirestore();
+    const profile = getState().firebase.profile;
+    const authorId = getState().firebase.auth.uid
     firestore.collection('wines').add({
       ...wine,
-      authorFirstName: 'Victoria',
-      authorLastName: 'Martinez',
-      authorID: 12345,
+      authorFirstName: profile.firstName,
+      authorLastName: profile.lastName,
+      authorID: authorId,
       createdAt: new Date()
     }).then(() => {
       dispatch({type: 'CREATE_WINE', wine: wine });
